8ff2b3fb1a1effb7a7a1029a02346cee47563e90
Rafael Martins <rafael.martins92@outlook.com>
2023-03-20 23:24:37 -0300

    feat: install gulp autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
 
 function compilaSass() {
   return gulp
     .src("scss/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+        cascade: false,
+      })
+    )
     .pipe(gulp.dest("css/"));
 }
 
