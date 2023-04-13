bfff60aa2b818719f11a35cb8f54f40fc1a01a72
Rafael Martins <rafael.martins92@outlook.com>
2023-04-13 20:39:27 -0300

    feat: add progress bar

--- javascript/scripts/hero.js
+++ javascript/scripts/hero.js
@@ -13,6 +13,8 @@ const slideThumbnail = new Swiper(".slide-thumbnail", {
   },
 });
 
+const progressSlide = document.querySelector(".js-progress");
+
 const slideHero = new Swiper(".slide-principal", {
   effect: "fade",
   thumbs: {
@@ -21,5 +23,21 @@ const slideHero = new Swiper(".slide-principal", {
   autoplay: {
     delay: 6000,
     disabledOnInteraction: false,
+  },
+  on: {
+    init: function () {
+      progressSlide.classList.remove("animate");
+      progressSlide.classList.remove("active");
+      progressSlide.classList.add("animate");
+      progressSlide.classList.add("active");
+    },
+    slideChangeTransitionStart: function () {
+      progressSlide.classList.remove("animate");
+      progressSlide.classList.remove("active");
+      progressSlide.classList.add("active");
+    },
+    slideChangeTransitionEnd: function () {
+      progressSlide.classList.add("animate");
+    },
   },
 });
